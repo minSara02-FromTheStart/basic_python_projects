0b5a6fd04633a8933b42ae419ff7aadb84a81f38
Tasnim Sara <tsn.min02@gmail.com>
2026-02-04 22:33:12 +0600

    Student Add,View,Search,Exit

---
+++ 02_Student_Management_System/system.py
@@ -0,0 +1,61 @@
+
+print("Welcome to the Student Management System!!!")
+print("--------------------------------------------")
+
+students = []
+def Add_student():
+    student = {}
+    student['ID'] = input("Enter Student ID: ")
+    student['Name'] = input("Enter Student Name: ")
+    student['Age'] = input("Enter Student Age: ")
+    student['Class'] = input("Enter Student Class: ")
+    students.append(student)
+    print("Student added successfully!\n")
+
+def View_students():
+    if not students:
+        print("No students found.\n")
+        return
+    print("\nList of Students:")
+    for student in students:
+        print(f"ID: {student['ID']}, Name: {student['Name']}, Age: {student['Age']}, Class: {student['Class']}")
+    print()
+
+def Search_student():
+    search_id = input("Enter Student ID to search: ")
+
+    if not students:
+        print("No students found.\n")
+        return
+
+    for student in students:
+        if student['ID'] == search_id:
+            print("\nStudent Found:")
+            print(f"ID: {student['ID']}, Name: {student['Name']}, Age: {student['Age']}, Class: {student['Class']}\n")
+            return
+
+    # runs only if loop finishes without finding
+    print("Student not found.\n")
+
+        
+def main():
+    while True:
+        print("1. Add Student")
+        print("2. View Students")
+        print("3. Search Student")
+        print("4. Exit")
+        choice = input("Enter your choice (1-4): ")
+        if choice == '1':
+            Add_student()
+        elif choice == '2':
+            View_students()
+        elif choice == '3':
+            Search_student()
+        elif choice == '4':
+            print("Exiting the Student Management System. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.\n")
+main()
+        
+    
