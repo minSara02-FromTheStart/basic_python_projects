dd0ea7ce0806503fcbf91977f3b98524765ec7ce
Tasnim Sara <tsn.min02@gmail.com>
2026-02-04 22:06:34 +0600

    Student Add,View,Search,Exit

--- 02_Student_Management_System/student_Management_system.py
+++ 02_Student_Management_System/student_Management_system.py
@@ -21,19 +21,36 @@ def View_students():
         print(f"ID: {student['ID']}, Name: {student['Name']}, Age: {student['Age']}, Class: {student['Class']}")
     print()
 
-
+def Search_student():
+    search_ID = input("Enter Student ID to search: ")
+    for student in students:
+        if not students:
+            print("No students found.")
+            return
+        else:
+            if student['ID'] == search_ID:
+                print(f"Student Found: ID: {student['ID']}, Name: {student['Name']}, Age: {student['Age']}, Class: {student['Class']}\n")
+                return
+            else:
+                print("Student not found.")
+                return
+        return
+           
 
 def main():
     while True:
         print("1. Add Student")
         print("2. View Students")
-        print("3. Exit")
-        choice = input("Enter your choice (1-3): ")
+        print("3. Search Student")
+        print("4. Exit")
+        choice = input("Enter your choice (1-4): ")
         if choice == '1':
             Add_student()
         elif choice == '2':
             View_students()
         elif choice == '3':
+            Search_student()
+        elif choice == '4':
             print("Exiting the Student Management System. Goodbye!")
             break
         else:
